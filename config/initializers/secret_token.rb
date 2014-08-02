33cd0bd95cb7341ef259dd387c5f9924db96f217
Clement007 <ambitieux.clement@gmail.com>
2014-08-02 12:08:31 +0200

    Struggled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fansheaven::Application.config.secret_token = 'c972cface8234837f91a0bcad283dc5636996b59699a4d79852b72a64f76eea771d22e6f5d3cb45701207ef7bd0b1db378c5cf11542867b1041a17f688d9f90e'
