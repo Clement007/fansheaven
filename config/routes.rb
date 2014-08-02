3ee27b943091e0c0a0f4ad1accc8c19edfff4ff3
Clement007 <ambitieux.clement@gmail.com>
2014-08-02 14:46:49 +0200

    Paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Fansheaven::Application.routes.draw do
   root :to => "pages#home"
-
   get 'pages/home'
 
+  match '/contact',   :to => 'pages#contact'
+  match '/about',     :to => 'pages#about'
+  match '/help',      :to => 'pages#help'
+  match '/analytics', :to => 'pages#analytics'
 
-  get 'users/new'
-  get 'pages/analytics'
-  get 'pages/help'
-  get 'pages/about'
-  get 'pages/contact'
+  # get 'users/new'
+  # get 'pages/analytics'
+  # get 'pages/help'
+  # get 'pages/about'
+  # #get 'pages/contact'
 
-  # match '/analytics', :to => 'pages#analytics'
-  # match '/help', :to => 'pages#help'
-  # match '/about', :to => 'pages#about'
-  # match '/contact', :to => 'pages#contact'
+
 end
