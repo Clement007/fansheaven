8635162226f245eed3372f6227b2d11c41b49a8d
Clement007 <ambitieux.clement@gmail.com>
2014-08-06 12:44:06 +0200

    Struggle resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user  = User.new
     @title = "Sign up"
+  end
+
+  def create
+    # raise params[:user].inspect
+    @user  = User.new(params[:user])
+    if @user.save
+       # flash[:success] = "Welcome to Fans Heaven App!"
+        redirect_to @user, :flash => {:success => "Welcome to Fans Heaven App!" }
+    #   Handle the comment save
+      else
+    @title = "Sign up"
+    render 'new'
+      end
   end
 end
