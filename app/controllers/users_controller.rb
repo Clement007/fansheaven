fd56f4f96990421fb23ff440534c3dc92f5a6264
Clement007 <ambitieux.clement@gmail.com>
2014-08-11 19:28:45 +0200

    Do not why

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def show
-     @user = User.find(params[:id])
-     @title = @user.name
+    @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def new
@@ -15,12 +16,41 @@ class UsersController < ApplicationController
     @user  = User.new(params[:user])
     if @user.save
       sign_in @user
-       # flash[:success] = "Welcome to Fans Heaven App!"
-        redirect_to @user, :flash => {:success => "Welcome to Fans Heaven App!" }
-    #   Handle the comment save
-      else
-    @title = "Sign up"
-    render 'new'
-      end
+      # flash[:success] = "Welcome to Fans Heaven App!"
+      redirect_to @user, :flash => { :success => "Welcome to Fans Heaven App!" }
+      #   Handle the comment save
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile Updated." }
+    else
+
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless @user == current_user?(@user)
   end
 end
+
+
