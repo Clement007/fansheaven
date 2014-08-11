0363a5b3da42fd728c9ccc9e4fc433ff43449700
Clement007 <ambitieux.clement@gmail.com>
2014-08-11 18:04:18 +0200

    Still problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-before_filter :authenticate, :only => [:edit, :update]
-before_filter :correct_user, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def show
-     @user = User.find(params[:id])
-     @title = @user.name
+    @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def new
@@ -16,13 +16,13 @@ before_filter :correct_user, :only => [:edit, :update]
     @user  = User.new(params[:user])
     if @user.save
       sign_in @user
-       # flash[:success] = "Welcome to Fans Heaven App!"
-        redirect_to @user, :flash => { :success => "Welcome to Fans Heaven App!" }
-    #   Handle the comment save
-      else
-    @title = "Sign up"
-    render 'new'
-      end
+      # flash[:success] = "Welcome to Fans Heaven App!"
+      redirect_to @user, :flash => { :success => "Welcome to Fans Heaven App!" }
+      #   Handle the comment save
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
   def edit
@@ -36,15 +36,15 @@ before_filter :correct_user, :only => [:edit, :update]
       redirect_to @user, :flash => { :success => "Profile Updated." }
     else
 
-    @title = "Edit user"
-    render 'edit'
+      @title = "Edit user"
+      render 'edit'
     end
   end
 
   private
 
   def authenticate
-   deny_access unless signed_in?
+    deny_access unless signed_in?
   end
 
   def correct_user
