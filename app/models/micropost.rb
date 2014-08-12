77cef55714acf8dd506b3d016494beedc36c4bb4
Clement007 <ambitieux.clement@gmail.com>
2014-08-12 22:52:08 +0200

    Micropsots validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,5 +13,6 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
 
+  validates :content, :presence => true, :length =>  { :maximum => 140 }
   default_scope :order => 'microposts.created_at DESC'
   end
